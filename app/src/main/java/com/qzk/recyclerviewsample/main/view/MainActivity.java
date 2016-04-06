ab5c175b35761b7c48b57617f98a2400e1cd7e21
qinzongke <975578110@qq.com>
2016-04-06 13:48:37 +0800

    init prejrect

---
+++ app/src/main/java/com/qzk/recyclerviewsample/main/view/MainActivity.java
@@ -0,0 +1,20 @@
+package com.qzk.recyclerviewsample.main.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.qzk.recyclerviewsample.R;
+
+/**
+ * 当前类注释：
+ * 项目名：RecyclerViewSample
+ * 包名： com.qzk.recyclerviewsample.main.view
+ * Created by QZK on 2016/4/6.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
